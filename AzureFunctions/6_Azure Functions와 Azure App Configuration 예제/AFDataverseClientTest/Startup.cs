20d6460bbeb6964a54c5c79446a4fc6cf94603d5
woochang <nanenchanga53@gmail.com>
2022-07-04 14:51:26 +0900

    테스트용

---
+++ AzureFunctions/6_Azure Functions와 Azure App Configuration 예제/AFDataverseClientTest/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+[assembly: FunctionsStartup(typeof(AFDataverseClientTest.Startup))]
+
+namespace AFDataverseClientTest
+{
+    internal class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            string cs = Environment.GetEnvironmentVariable("ACCS");
+            
+            builder.ConfigurationBuilder
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("local.settings.json", optional: false, reloadOnChange: false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.ConfigurationBuilder.AddAzureAppConfiguration(cs);
+            
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            
+        }
+    }
+}
